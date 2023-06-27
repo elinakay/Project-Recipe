3b5d15b67b0efba63a3c800f3d0c315cda7e4fd6
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2023-06-26 17:07:08 -0700

    Updated RecipeCreate.js
    Updated the file so that the photo field only accepts a valid URL.

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -6,9 +6,16 @@ function RecipeCreate({ addRecipe }) {
   const [photo, setPhoto] = useState("");
   const [ingredients, setIngredients] = useState("");
   const [preparation, setPreparation] = useState("");
+  const [photoError, setPhotoError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Validate photo URL
+    if (!validateURL(photo)) {
+      setPhotoError("Invalid URL");
+      return;
+    }
 
     const newRecipe = {
       name,
@@ -25,92 +32,80 @@ function RecipeCreate({ addRecipe }) {
     setPhoto("");
     setIngredients("");
     setPreparation("");
-  }
+    setPhotoError("");
+  };
 
-  
+  const validateURL = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
   return (
     <form name="create" onSubmit={handleSubmit}>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <label htmlFor="name">Name:</label>
-            </td>
-            <td>
-              <input
-              type="text"
-              name="name"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-            </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="cuisine">Cuisine:</label>
-              </td>
-              <td>
-                <input
-                type="text"
-                name="cuisine"
-                id="cuisine"
-                value={cuisine}
-                onChange={(e) => setCuisine(e.target.value)}
-                required
-                />
-              </td>
-              </tr>
-              <tr>
-                <td>
-                  <label htmlFor="photo">Photo URL:</label>
-                </td>
-                <td>
-                  <input
-                  type="text"
-                  name="photo"
-                  id="photo"
-                  value={photo}
-                  onChange={(e) => setPhoto(e.target.value)}
-                  required
-                  />
-                </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label htmlFor="ingredients">Ingredients:</label>
-                  </td>
-                  <td>
-                    <textarea
-                    name="ingredients"
-                    id="ingredients"
-                    value={ingredients}
-                    onChange={(e) => setIngredients(e.target.value)}
-                    required
-                    ></textarea>
-                  </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <textarea
-                      name="preparation"
-                      id="preparation"
-                      value={preparation}
-                      onChange={(e) => setPreparation(e.target.value)}
-                      required
-                      ></textarea>
-                    </td>
-                    </tr>
-                    <tr>
-            <td>
-              <button type="submit">Create</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="form-row">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="cuisine">Cuisine:</label>
+        <input
+          type="text"
+          name="cuisine"
+          id="cuisine"
+          value={cuisine}
+          onChange={(e) => setCuisine(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="photo">Photo URL:</label>
+        <input
+          type="text"
+          name="photo"
+          id="photo"
+          value={photo}
+          onChange={(e) => setPhoto(e.target.value)}
+          required
+        />
+        {photoError && <span className="error-message">{photoError}</span>}
+      </div>
+      <div className="form-row">
+        <label htmlFor="ingredients">Ingredients:</label>
+        <textarea
+          name="ingredients"
+          id="ingredients"
+          value={ingredients}
+          onChange={(e) => setIngredients(e.target.value)}
+          required
+        ></textarea>
+      </div>
+      <div className="form-row">
+        <label htmlFor="preparation">Preparation:</label>
+        <textarea
+          name="preparation"
+          id="preparation"
+          value={preparation}
+          onChange={(e) => setPreparation(e.target.value)}
+          required
+        ></textarea>
+      </div>
+      <div className="form-row">
+        <button type="submit">Create</button>
+      </div>
     </form>
   );
 }
 
 export default RecipeCreate;
+
